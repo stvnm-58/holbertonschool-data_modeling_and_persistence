617d12ef7a545502adba781a2060bedbf608b08c
steven <12992@holbertonstudents.com>
2026-06-03 11:57:11 +0200

    correction

--- sql_crud/10-select_tech_books.sql
+++ sql_crud/10-select_tech_books.sql
@@ -1,1 +1,1 @@
-SELECT title, price, stock FROM books WHERE genre = 'tech' and published_year >= 2000;
+SELECT title, price, stock FROM books WHERE published_year >= 2000 AND genre = 'Tech';
--- sql_crud/10-stock_by_genre.sql
+++ sql_crud/10-stock_by_genre.sql
@@ -1,1 +1,1 @@
-SELECT genre, sum(stock) FROM books GROUP BY genre;
+SELECT genre, SUM(stock) FROM books GROUP BY genre;
