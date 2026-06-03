b75a6b48d7cc0494148296a446eef0441f80e787
steven <12992@holbertonstudents.com>
2026-06-03 14:49:38 +0200

    correction

--- sql_join/1-books-with-authors.sql
+++ sql_join/1-books-with-authors.sql
@@ -1,4 +1,4 @@
-SELECT title, name AS author_name
-FROM books
-INNER JOIN autor ON books.author_id = autor.author_id
+SELECT title, name AS author_name 
+FROM books 
+INNER JOIN authors ON books.author_id = authors.id 
 ORDER BY title ASC;
